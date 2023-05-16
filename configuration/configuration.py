13b3efa0afa119c83e9e2f521aa2e38e4810cfa5
Alexander Hering <dev.hering@gmail.com>
2023-05-16 18:08:36 +0200

    adding in configuration base

---
+++ configuration/configuration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                model_data_handler                 
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import paths as PATHS
+from dotenv import load_dotenv
+
+load_dotenv()
---
+++ configuration/paths.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                model_data_handler                 
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import os
+
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
