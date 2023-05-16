fac7bc736489a70ba6a3e7251144df3face3c5ef
Alexander Hering <dev.hering@gmail.com>
2023-05-16 18:28:18 +0200

    added and secured paths

--- configuration/paths.py
+++ configuration/paths.py
@@ -10,5 +10,9 @@ import os
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 DATA_PATH = os.path.join(BASE_PATH, "data")
-if not os.path.exists(DATA_PATH):
-    os.makedirs(DATA_PATH)
+TEMP_DATA_PATH = os.path.join(BASE_PATH, "data", "temp")
+
+
+for paths in [DATA_PATH, TEMP_DATA_PATH]:
+    if not os.path.exists(DATA_PATH):
+        os.makedirs(paths)
