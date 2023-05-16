796837085564b7e99e72e5d2946e0747f456f3fd
Alexander Hering <dev.hering@gmail.com>
2023-05-16 19:23:45 +0200

    implemented abstract methods

--- model/civitai_api_wrapper.py
+++ model/civitai_api_wrapper.py
@@ -5,7 +5,10 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+import requests
+import json
 from typing import Any, Optional, List
+from ..configuration import configuration as cfg
 from abstract_api_wrapper import AbstractAPIWrapper
 
 class CivitaiAbstractAPIWrapper(AbstractAPIWrapper):
@@ -16,7 +19,11 @@ class CivitaiAbstractAPIWrapper(AbstractAPIWrapper):
         """
         Initiation method.
         """
-        pass
+        self.base_url = "https://civitai.com/"
+        self.api_base_url = "https://civitai.com/api/v1/"
+        self.model_by_versionhash_url = "https://civitai.com/api/v1/model-versions/by-hash/"
+        self.model_by_id_url = "https://civitai.com/api/v1/models/"
+
 
     def check_connection(self, *args: Optional[List], **kwargs: Optional[dict]) -> bool:
         """
@@ -25,7 +32,7 @@ class CivitaiAbstractAPIWrapper(AbstractAPIWrapper):
         :param kwargs: Arbitrary keyword arguments.
         :return: True if connection was established successfuly else False.
         """
-        pass
+        return requests.get(self.base_url).status_code == 200
 
     def collect_metadata(self, identifier: str, model_id: Any, *args: Optional[List], **kwargs: Optional[dict]) -> dict:
         """
@@ -36,4 +43,10 @@ class CivitaiAbstractAPIWrapper(AbstractAPIWrapper):
         :param kwargs: Arbitrary keyword arguments.
         :return: Metadata for given model ID.
         """
-        pass
+        resp = requests.get({"hash": self.model_by_versionhash_url, "id": self.model_by_id_url}[identifier] + str(model_id), headers={"Authorization": cfg.CIVITAI_API_KEY})
+        try:
+            meta_data = json.loads(resp.content)
+            if meta_data is not None and not "error" in meta_data:
+                return meta_data
+        except json.JSONDecodeError:
+                return {}
