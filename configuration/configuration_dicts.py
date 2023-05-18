7d2dcd7505abd61e45d4045f0f08449a9d8db81d
Alexander Hering <dev.hering@gmail.com>
2023-05-18 15:53:14 +0200

    starting civitai folder config

--- configuration/configuration_dicts.py
+++ configuration/configuration_dicts.py
@@ -8,4 +8,120 @@
 """
 Dictionary based configuration
 """
-CIVITAI_FOLDER_STRUCTURE = {}
+CIVITAI_FOLDER_STRUCTURE = {
+            "BLIP": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "BSRGAN": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "CHECKPOINTS": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "CODEFORMER": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "CONTROL_NET": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "DEEPBOORU": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "EMBEDDINGS": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "ESRGAN": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "GFPGAN": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "HYPERNETWORKS": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "KARLO": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "LDSR": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "LORA": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "LYCORIS": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "POSES": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "REAL_ESRGAN": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "SCUNET": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "STABLE_DIFFUSION": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "SWINIR": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "TEXTUAL_INVERSION": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "TORCH_DEEPDANBOORU": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "VAE": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            },
+            "WILDCARDS": {
+                "sort_into": lambda _: False,
+                "not_sort_into": lambda _: True,
+                "subfolders": {}
+            }
+        }
