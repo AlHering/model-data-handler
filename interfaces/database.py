2d73473f4e05c5a141333e76295211f04c293874
Alexander Hering <dev.hering@gmail.com>
2023-05-19 18:15:19 +0200

    starting interfaces with database interface

---
+++ interfaces/database.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                model_data_handler                 
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+from logging import Logger
+from typing import Any, Optional, List
+from ..configuration import configuration as cfg
+
+
+class Database(object):
+    """
+    Class, representing database.
+    """
+    def __init__(self, profile: dict = None) -> None:
+        pass
